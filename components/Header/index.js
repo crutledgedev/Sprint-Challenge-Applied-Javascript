c3159ae83e76a0ad545d5b0dc5897a74c1dfcd76
Chance Rutledge <chancerutledge2@gmail.com>
2019-11-01 10:08:42 -0700

    header and tabs working

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,41 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+
+ 
+function Header() {
+
+
+const header = document.createElement('div');
+const date = document.createElement('span');
+const lambdatimesh1 = document.createElement('h1');
+const temp = document.createElement('span');
+;
+
+//set content
+date.textContent = `Smarch 28, 2019`;
+lambdatimesh1.textContent = `Lambda Times`;
+temp.textContent = `98°`;
+
+//class
+header.classList.add('header');
+date.classList.add('date');
+temp.classList.add('temp')
+
+
+//structure
+header.appendChild(date);
+header.appendChild(lambdatimesh1);
+header.appendChild(temp);
+
+
+
+//console.log(addHeader);
+return header;
+}
+// console.log(Header.appendChild());
+const addHeader = document.querySelector('.header-container');
+addHeader.appendChild(Header());
+
+//console.log(addHeader);
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+//console.log(response.data.topics);
+const lsTopics = document.querySelector('.topics');
+return response.data.topics.forEach(e =>{
+    lsTopics.appendChild(tabs(e));
+})
+
+})
+.catch(error => {
+    console.log(error);
+})
+
+
+function tabs(topics){
+    //create
+    const tab = document.createElement('div');
+    //set content
+    tab.textContent = topics;
+    //class
+    tab.classList.add('tab');
+    return tab;
+
+
+
+
+
+
+
+}
